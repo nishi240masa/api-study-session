fb292a2b6d6088f9f031e1307112fe0f5e5eb38c
nishi240masa <masa2401106@gmail.com>
2024-12-04 16:10:13 +0900

    chore: Update CORS header in GET route for root path

--- src/app.js
+++ src/app.js
@@ -17,77 +17,23 @@ const router = createRouter();
 app.use(router);
 
 // GET,POST,PUT,DELETE
-// GET: リソースの取得
-// POST: リソースの作成
-// PUT: リソースの更新
-// DELETE: リソースの削除
 
 // ここでGETリクエストを受け取る
 
 router.get(
   "/",
   defineEventHandler((event) => {
-    setResponseHeader(event, "Access-Control-Allow-Origin", "*");
+    setResponseHeader(event, "Access-Control-Allow-Origin", "*"); //CORS対策
     return { message: "これはgetの処理" };
   })
 );
 
-router
-  .get(
-    "/test",
-    defineEventHandler((event) => {
-      setResponseHeader(event, "Access-Control-Allow-Origin", "*");
-      return { message: "これはgetの処理" };
-    })
-  )
-  .post(
-    "/test",
-    defineEventHandler((event) => {
-      setResponseHeader(event, "Access-Control-Allow-Origin", "*");
 
-      // ランダムの数値を生成して配列に格納
-      const random = Math.floor(Math.random() * 10);
-      const array = [];
-      for (let i = 0; i < random; i++) {
-        array.push(i);
-      }
-      return { array };
-    })
-  )
-  .put(
-    "/test",
-    defineEventHandler((event) => {
-      setResponseHeader(event, "Access-Control-Allow-Origin", "*");
 
-      // クエリパラメータから名前を取得
-      const query = getQuery(event);
-    })
-  )
-  .delete(
-    "/test",
-    defineEventHandler((event) => {
-      setResponseHeader(event, "Access-Control-Allow-Origin", "*");
-      return { message: "Hello, World!" };
-    })
-  );
+// GET: リソースの取得
 
-// エンドポイントの設定
-router.get(
-  "/omikuji", //これがエンドポイント
-  defineEventHandler((event) => {
-    setResponseHeader(event, "Access-Control-Allow-Origin", "*"); // CORSを許可する
-    // クエリパラメータから誕生日を取得
-    const query = getQuery(event);
-    const birthday = query.birthday;
-    if (!birthday) {
-      return {
-        error:
-          "誕生日を指定してください。?birthday=YYYY-MM-DD の形式で指定してください。",
-      };
-    }
+// POST: リソースの作成
 
-    // おみくじの結果を生成
-    const fortune = getFortune();
-    return { birthday, fortune };
-  })
-);
+// PUT: リソースの更新
+
+// DELETE: リソースの削除
