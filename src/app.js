a0751e69e5c1aba708d2f39bc05cd33ff274c314
nishi240masa <masa2401106@gmail.com>
2024-12-04 16:11:17 +0900

    chore: Update GET route for root path to include CORS header

--- src/app.js
+++ src/app.js
@@ -17,23 +17,77 @@ const router = createRouter();
 app.use(router);
 
 // GET,POST,PUT,DELETE
+// GET: リソースの取得
+// POST: リソースの作成
+// PUT: リソースの更新
+// DELETE: リソースの削除
 
 // ここでGETリクエストを受け取る
 
 router.get(
   "/",
   defineEventHandler((event) => {
-    setResponseHeader(event, "Access-Control-Allow-Origin", "*"); //CORS対策
+    setResponseHeader(event, "Access-Control-Allow-Origin", "*");
     return { message: "これはgetの処理" };
   })
 );
 
+router
+  .get(
+    "/test",
+    defineEventHandler((event) => {
+      setResponseHeader(event, "Access-Control-Allow-Origin", "*");
+      return { message: "これはgetの処理" };
+    })
+  )
+  .post(
+    "/test",
+    defineEventHandler((event) => {
+      setResponseHeader(event, "Access-Control-Allow-Origin", "*");
 
+      // ランダムの数値を生成して配列に格納
+      const random = Math.floor(Math.random() * 10);
+      const array = [];
+      for (let i = 0; i < random; i++) {
+        array.push(i);
+      }
+      return { array };
+    })
+  )
+  .put(
+    "/test",
+    defineEventHandler((event) => {
+      setResponseHeader(event, "Access-Control-Allow-Origin", "*");
 
-// GET: リソースの取得
+      // クエリパラメータから名前を取得
+      const query = getQuery(event);
+    })
+  )
+  .delete(
+    "/test",
+    defineEventHandler((event) => {
+      setResponseHeader(event, "Access-Control-Allow-Origin", "*");
+      return { message: "Hello, World!" };
+    })
+  );
 
-// POST: リソースの作成
+// エンドポイントの設定
+router.get(
+  "/omikuji", //これがエンドポイント
+  defineEventHandler((event) => {
+    setResponseHeader(event, "Access-Control-Allow-Origin", "*"); // CORSを許可する
+    // クエリパラメータから誕生日を取得
+    const query = getQuery(event);
+    const birthday = query.birthday;
+    if (!birthday) {
+      return {
+        error:
+          "誕生日を指定してください。?birthday=YYYY-MM-DD の形式で指定してください。",
+      };
+    }
 
-// PUT: リソースの更新
-
-// DELETE: リソースの削除
+    // おみくじの結果を生成
+    const fortune = getFortune();
+    return { birthday, fortune };
+  })
+);
